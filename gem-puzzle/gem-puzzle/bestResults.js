224e8f5063ea88c23eedf79e56e4641e5ca60f65
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-11-18 19:31:33 +0300

    update app

--- gem-puzzle/gem-puzzle/bestResults.js
+++ gem-puzzle/gem-puzzle/bestResults.js
@@ -11,10 +11,17 @@ function getBestResults() {
   const ifNobody = '<div class="table-item table-noitem">No results yet</div>';
   let resString = [];
   if (obj.length > 0) {
-    resString = obj.map((i, index) => {
-      const number = index < 9 ? `0${index + 1}` : `${index + 1}`;
-      return `<div class="table-item"><span>${number}. </span><span>Type: ${i.type} </span><span>Time: ${i.time}</span><span>Steps: ${i.clicks.split(':')[1]}</span></div>`;
-    });
+    resString = obj
+      .map((i) => {
+        const b = i;
+        b.clicks = parseInt(i.clicks.split(':')[1], 10);
+        return i;
+      })
+      .sort((a, b) => a.clicks - b.clicks)
+      .map(({ type, time, clicks }, index) => {
+        const number = index < 9 ? `0${index + 1}` : `${index + 1}`;
+        return `<div class="table-item"><span>${number}. </span><span>Type: ${type} </span><span>Time: ${time}</span><span>Steps: ${clicks}</span></div>`;
+      });
   }
   const returnValue = obj.length > 0 ? resString.join('') : ifNobody;
   table.innerHTML = returnValue;
