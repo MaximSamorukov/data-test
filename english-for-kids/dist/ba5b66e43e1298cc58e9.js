cfed03ee9b22f6d56b80c06fe7e821da869aaad0
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-12-02 12:17:10 +0300

    update app

---
+++ english-for-kids/dist/ba5b66e43e1298cc58e9.js
@@ -0,0 +1,26 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkenglish_for_kids"] = self["webpackChunkenglish_for_kids"] || []).push([["categories_tools_englishwords_js"],{
+
+/***/ "./categories/tools/englishwords.js":
+/*!******************************************!*\
+  !*** ./categories/tools/englishwords.js ***!
+  \******************************************/
+/*! namespace exports */
+/*! export words [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"words\": function() { return /* binding */ words; }\n/* harmony export */ });\nvar words = {\n  0: 'pliers',\n  1: 'hammer',\n  2: 'saw',\n  3: 'plane',\n  4: 'drill',\n  5: 'vise',\n  6: 'screwdriver',\n  7: 'awl',\n  8: 'flathead screwdriver',\n  9: 'wrench'\n};\n\n\n//# sourceURL=webpack://english-for-kids/./categories/tools/englishwords.js?");
+
+/***/ })
+
+}]);
