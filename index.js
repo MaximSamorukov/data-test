4a7efc8362d100559244449196e49170dab38452
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-01-01 23:52:41 +0300

    made some changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Reveal from "/node_modules/reveal.js";
+import Markdown from "/node_modules/reveal.js/plugin/markdown/markdown.esm.js";
 
 let deck = new Reveal({
   plugins: [Markdown]
