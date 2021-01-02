7df7b0526f8fdf6cc206d85282305e8724315c07
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-01-02 20:34:27 +0300

    add first slide

--- slides/slide1.js
+++ slides/slide1.js
@@ -12,6 +12,20 @@ export default (number) => {
 
   titleElement.textContent = `Slide: ${number}`;
 
+  const bodyTop = document.createElement('div');
+  const bodyMiddle = document.createElement('div');
+  const bodyFooter = document.createElement('div');
+
+  bodyTop.className = 'slide1-body-top';
+  bodyMiddle.className = 'slide1-body-middle';
+  bodyFooter.className = 'slide1-body-footer';
+
+  bodyElement.appendChild(bodyTop);
+  bodyElement.appendChild(bodyMiddle);
+  bodyElement.appendChild(bodyFooter);
+
+  bodyMiddle.innerHTML = "<span>Presentation:</span><br><span>«State management in<br>React - applications»</span>"
+  bodyFooter.textContent = "Samorukov M.L."
   container.appendChild(titleElement);
   container.appendChild(bodyElement);
   container.appendChild(footerElement);
